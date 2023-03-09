df501c2b3ad4e903aa3f1aca1fc06ccb68a32a07
illumi420 <illumi9@protonmail.com>
2023-03-09 20:37:47 +0100

    home made decimal to binary converting function. returns 'second hex digit from first hex block of an MAC address' in a four bit binary format

---
+++ bla.py
@@ -0,0 +1,40 @@
+
+def decToBin(decimal_number):
+    bin_format = ""
+    
+    if decimal_number <= 0:
+        bin_format += "0000"
+    elif decimal_number == 1:
+        bin_format += "0001"
+    else:
+        bin_format += str(decToBin(decimal_number // 2))
+        rest = decimal_number % 2
+        bin_format += str(rest)
+    
+    
+    return bin_format
+
+
+def mac2linklocal():
+    mac = input("Please Enter MAC-Address: ")
+    first_hex_block = mac[0] + mac[1]
+    second_nibble_in_dec = int(first_hex_block[1], 16)
+    second_nibble_in_bin = decToBin(second_nibble_in_dec)[-4:]
+    
+   
+    
+    # second_4_bits = int(mac[1], 16)
+    # print(second_4_bits)
+    # second_bin = decToBin(second_4_bits)
+    # print(second_bin)
+    
+    
+    
+print("Please Select an Option: ")
+print("1. MAC-Address to IPv6 Link-Local")
+print("2. IPv6 Link-Local to MAC-Address")
+
+select = input("Select: ")
+
+if select == "1":
+    print(mac2linklocal())    
